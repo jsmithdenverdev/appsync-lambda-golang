b8af65ca93c190f94bed3f4035bbbd606daff6fb
Jacob Smith <jsmithdenverdev@gmail.com>
2024-07-10 15:15:46 -0600

    stub out read-item

--- cmd/read-item/main.go
+++ cmd/read-item/main.go
@@ -2,13 +2,66 @@ package main
 
 import (
 	"context"
-	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/jsmithdenverdev/appsync-lambda-golang/internal/handlers"
 )
 
+var (
+	logger         *slog.Logger
+	dynamodbclient *dynamodb.Client
+	cfg            handlers.HandleReadItemConfig
+)
+
+const (
+	envTableName = "TABLE_NAME"
+)
+
+func init() {
+	var missingcfg []string
+
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	// Load default AWS config
+	// If loading fails log an error and exit
+	awscfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load default aws config: %s", err.Error())
+		os.Exit(1)
+		return
+	}
+
+	// Initialize dynamodb client
+	dynamodbclient = dynamodb.NewFromConfig(awscfg)
+
+	// Load environment variables
+	cfg.TableName = os.Getenv(envTableName)
+	if cfg.TableName == "" {
+		missingcfg = append(missingcfg, envTableName)
+	}
+
+	// If any environment variables are missing log an error and exit
+	if len(missingcfg) > 0 {
+		fmt.Fprintf(
+			os.Stderr,
+			"failed to load the following environment variables: %s",
+			strings.Join(missingcfg, ", "),
+		)
+		os.Exit(1)
+		return
+	}
+}
+
 func main() {
-	lambda.Start(func(ctx context.Context) error {
-		return errors.New("not implemented")
-	})
+	lambda.Start(handlers.HandleReadItem(
+		cfg,
+		logger,
+		dynamodbclient,
+	))
 }
--- internal/handlers/read_item.go
+++ internal/handlers/read_item.go
@@ -1,1 +1,36 @@
 package handlers
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/jsmithdenverdev/appsync-lambda-golang/internal/responses"
+)
+
+// HandleReadItemConfig represents the config needed by the ReadItem Lambda
+// function.
+type HandleReadItemConfig struct {
+	TableName string
+}
+
+// handleReadItemDynamoDBClient represents the Dynamo DB operations that can
+// be performed from HandleReadItem.
+type handleReadItemDynamoDBClient interface {
+	GetItem(ctx context.Context, params *dynamodb.GetItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error)
+}
+
+// HandleReadItem is the handler for the ReadItem Lambda function.
+func HandleReadItem(
+	config HandleReadItemConfig,
+	logger *slog.Logger,
+	dynamodbclient handleReadItemDynamoDBClient,
+) func(ctx context.Context, ids []string) ([]responses.Item, error) {
+	return func(ctx context.Context, ids []string) ([]responses.Item, error) {
+		var response []responses.Item
+
+		logger.InfoContext(ctx, "read item", "ids", ids)
+
+		return response, nil
+	}
+}
