ec692080fe8e18c0ca97b053963993a317badd8e
Jacob Smith <jsmithdenverdev@gmail.com>
2024-07-11 08:35:33 -0600

    pushin the stuff

--- internal/handlers/read_item.go
+++ internal/handlers/read_item.go
@@ -2,10 +2,11 @@ package handlers
 
 import (
 	"context"
-	"encoding/json"
 	"log/slog"
 
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/jsmithdenverdev/appsync-lambda-golang/internal/models"
 )
 
@@ -18,7 +19,7 @@ type HandleReadItemConfig struct {
 // handleReadItemDynamoDBClient represents the Dynamo DB operations that can
 // be performed from HandleReadItem.
 type handleReadItemDynamoDBClient interface {
-	GetItem(ctx context.Context, params *dynamodb.GetItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error)
+	BatchGetItem(ctx context.Context, params *dynamodb.BatchGetItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.BatchGetItemOutput, error)
 }
 
 // readItemRequest represents a request to read an item. The lambda function is
@@ -49,18 +50,51 @@ func HandleReadItem(
 ) func(ctx context.Context, reqs []request[readItemRequest]) ([]readItemResponse, error) {
 	return func(ctx context.Context, reqs []request[readItemRequest]) ([]readItemResponse, error) {
 		responses := make([]readItemResponse, len(reqs))
+		keys := make([]map[string]types.AttributeValue, len(reqs))
 
 		logger.InfoContext(ctx, "read item", "request", reqs)
 
 		for i, req := range reqs {
 			if problems := req.Valid(ctx); len(problems) > 0 {
 				responses[i].Error = errInvalidRequest[readItemRequest]{ctx, req}
+			} else {
+				keys[i] = map[string]types.AttributeValue{
+					"id": &types.AttributeValueMemberS{Value: req.Args.ID},
+				}
 			}
 		}
 
-		b, _ := json.MarshalIndent(responses, "", "\t")
+		batchGetInput := &dynamodb.BatchGetItemInput{
+			RequestItems: map[string]types.KeysAndAttributes{
+				config.TableName: {
+					Keys: keys,
+				},
+			},
+		}
 
-		logger.InfoContext(ctx, "responses", "responses", responses, "raw", string(b))
+		result, err := dynamodbclient.BatchGetItem(ctx, batchGetInput)
+		if err != nil {
+			logger.ErrorContext(ctx, "failed to batch get items", "error", err)
+			return responses, errInternalServer
+		}
+
+		if len(result.Responses[config.TableName]) != len(reqs) {
+			logger.ErrorContext(ctx, "data loader mismatch", "error", err)
+			return responses, errInternalServer
+		}
+
+		for i, row := range result.Responses[config.TableName] {
+			var item models.Item
+
+			if err := attributevalue.UnmarshalMap(row, &item); err != nil {
+				logger.ErrorContext(ctx, "failed to unmarshal item", "error", err)
+				responses[i] = readItemResponse{Error: errInternalServer}
+				continue
+			}
+
+			responses[i] = readItemResponse{Value: item}
+		}
+
 		return responses, nil
 	}
 }
